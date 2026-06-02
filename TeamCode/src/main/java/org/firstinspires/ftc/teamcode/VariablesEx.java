c541934e4acebe312117f69fe8345c0cb5c21c5a
anth8nyy <anthony.ilias16@gmail.com>
2026-06-02 21:29:25 +0300

    Create GamePadPractice
    Practice on Variables and gamepad telemetry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablesEx.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class VariablesEx extends OpMode {
+    @Override
+    public void init() {
+        int teamNumber = 23014;
+        double motorSpeed = 0.75;
+        boolean clawClosed = true;
+        int motorAngle = 150;
+        telemetry.addData("Team Number", teamNumber);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Claw State",clawClosed);
+        telemetry.addData("Motor Angle", motorAngle);
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
