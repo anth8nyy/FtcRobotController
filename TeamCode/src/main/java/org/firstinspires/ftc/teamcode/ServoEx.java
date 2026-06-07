5638abb4bcf6506b9bbc5bf039693f07253c3724
anth8nyy <anthony.ilias16@gmail.com>
2026-06-07 19:51:09 +0300

    Servo Practice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoEx.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.mechanisms.TestBench;
+import org.firstinspires.ftc.teamcode.mechanisms.TestBenchServo;
+
+@Disabled
+@TeleOp
+public class ServoEx extends OpMode {
+    TestBenchServo bench = new TestBenchServo();
+    double leftTrigger, rightTrigger;
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+        leftTrigger = 0.0;
+        rightTrigger = 0.0;
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.a){
+            bench.setServoPo(-1.0);
+        }
+        else {
+            bench.setServoPo(1.0);
+        }
+        if(gamepad1.b){
+            bench.setServoRot(1.0);
+        }
+        else{
+            bench.setServoRot(0);
+        }
+        leftTrigger = gamepad1.left_trigger;
+        rightTrigger = gamepad1.right_trigger;
+
+        bench.setServoPo(leftTrigger);
+        bench.setServoRot(rightTrigger);
+
+    }
+}
