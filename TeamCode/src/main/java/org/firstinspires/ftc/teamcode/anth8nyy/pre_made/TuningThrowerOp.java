bb8a04ca80795421b7c82b52c4fa0198e4447832
anth8nyy <anthony.ilias16@gmail.com>
2026-06-22 20:17:33 +0300

    tuning thrower

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anth8nyy/pre_made/TuningThrowerOp.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.anth8nyy.pre_made;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.anth8nyy.pre_made.samples.TuningThrower;
+
+@Disabled
+@TeleOp
+public class TuningThrowerOp extends OpMode {
+    TuningThrower thrower = new TuningThrower();
+    @Override
+    public void init() {
+        thrower.init(hardwareMap);
+    }
+    @Override
+    public void loop() {
+        thrower.thrower(gamepad1.yWasPressed(), gamepad1.bWasPressed(), gamepad1.dpadLeftWasPressed(), gamepad1.dpadRightWasPressed(), gamepad1.dpadUpWasPressed(), gamepad1.dpadDownWasPressed());
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anth8nyy/pre_made/samples/TuningThrower.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.anth8nyy.pre_made.samples;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+public class TuningThrower{
+    public DcMotorEx thrower;
+    public double highVelocity = 1500;
+    public double lowVelocity = 900;
+    double curTargetVelocity = highVelocity;
+    double F = 0;
+    double P = 0;
+    double[] stepSizes = {10.0,1.0,0.1,0.001,0.0001};
+    int stepIndex = 1;
+
+    public void init(HardwareMap hardwareMap){
+        thrower = hardwareMap.get(DcMotorEx.class,"motor");
+        thrower.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        thrower.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P,0,0,F);
+        thrower.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER,pidfCoefficients);
+    }
+    public void thrower(boolean y,boolean b, boolean dpadLeft, boolean dpadRight,boolean dpadUp,boolean dpadDown){
+        if (y){
+            if (curTargetVelocity == highVelocity){
+                curTargetVelocity = lowVelocity;
+            }
+        }
+        else {
+            curTargetVelocity = highVelocity;
+        }
+        if (b){
+            stepIndex = (stepIndex + 1) % stepSizes.length;
+        }
+        if (dpadLeft){
+            F += stepSizes[stepIndex];
+        }
+        if (dpadRight){
+            F -= stepSizes[stepIndex];
+        }
+        if (dpadUp){
+            P += stepSizes[stepIndex];
+        }
+        if (dpadDown){
+            P -= stepSizes[stepIndex];
+        }
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P,0,0,F);
+        thrower.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER,pidfCoefficients);
+        thrower.setVelocity(curTargetVelocity);
+        double curVelocity = thrower.getVelocity();
+        double error = curTargetVelocity - curVelocity;
+    }
+}
