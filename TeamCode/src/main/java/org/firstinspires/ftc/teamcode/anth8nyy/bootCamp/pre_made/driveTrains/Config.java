1f79e531068926cb911e5a0df37dfa38d9fda51e
anth8nyy <anthony.ilias16@gmail.com>
2026-07-02 00:29:16 +0300

    w

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/anth8nyy/bootCamp/pre_made/driveTrains/Config.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.anth8nyy.bootCamp.pre_made.driveTrains;
+
+public class Config {
+    public static String l_d ="left_drive";
+    public static String r_d = "right_drive";
+    public static String ramp = "ramp";
+    public static String intake = "intake";
+
+}
